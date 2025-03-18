6afaeabda8cd4eeb20e98fe028ecc3cdaf3d6579
fanqiliang <fanqiliang12@gmail.com>
2025-03-18 14:04:44 +0800

    update c2ascend

--- projects/c2ascend/gather_points_ascend.cpp
+++ projects/c2ascend/gather_points_ascend.cpp
@@ -1,5 +1,5 @@
 /**
- * @file gather_points.cpp
+ * @file add_custom.cpp
  *
  * Copyright (C) 2024. Huawei Technologies Co., Ltd. All rights reserved.
  *
@@ -13,16 +13,13 @@ class KernelGatherPoints {
 public:
   __aicore__ inline KernelGatherPoints() {}
   __aicore__ inline void Init(int32_t b, int32_t c, int32_t n, int32_t m,
-                              GM_ADDR points, GM_ADDR idx, GM_ADDR out) {
-    // int32_t BLOCK_LENGTH = c * n;
-    // int32_t IDX_BLOCK_LENGTH = m;
-    // int32_t OUT_BLOCK_LENGTH = c * m;
-    pointsGm.SetGlobalBuffer((__gm__ half *)points, b * c * n);
-    idxGm.SetGlobalBuffer((__gm__ half *)idx, b * m);
-    outGm.SetGlobalBuffer((__gm__ half *)out, b * c * m);
-    pipe.InitBuffer(inQueuePoints, 1, b * c * n * sizeof(half));
-    pipe.InitBuffer(inQueueIdx, 1, b * m * sizeof(half));
-    pipe.InitBuffer(outQueueOut, 1, b * c * m * sizeof(half));
+                              uint32_t *points, uint32_t *idx, uint32_t *out) {
+    pointsGm.SetGlobalBuffer((__gm__ int32_t *)points, b * c * n);
+    idxGm.SetGlobalBuffer((__gm__ int32_t *)idx, b * m);
+    outGm.SetGlobalBuffer((__gm__ int32_t *)out, b * c * m);
+    pipe.InitBuffer(inQueuePoints, 1, b * c * n * sizeof(int32_t));
+    pipe.InitBuffer(inQueueIdx, 1, b * m * sizeof(int32_t));
+    pipe.InitBuffer(outQueueOut, 1, b * c * m * sizeof(int32_t));
   }
   __aicore__ inline void Process(int32_t b, int32_t c, int32_t n, int32_t m) {
     int32_t loopCount = b * c * m;
@@ -36,8 +33,9 @@ public:
 private:
   __aicore__ inline void CopyIn(int32_t progress, int32_t b, int32_t c,
                                 int32_t n, int32_t m) {
-    AscendC::LocalTensor<half> pointsLocal = inQueuePoints.AllocTensor<half>();
-    AscendC::LocalTensor<half> idxLocal = inQueueIdx.AllocTensor<half>();
+    AscendC::LocalTensor<int32_t> pointsLocal =
+        inQueuePoints.AllocTensor<int32_t>();
+    AscendC::LocalTensor<int32_t> idxLocal = inQueueIdx.AllocTensor<int32_t>();
     AscendC::DataCopy(pointsLocal, pointsGm, b * c * n);
     AscendC::DataCopy(idxLocal, idxGm, b * m);
     inQueuePoints.EnQue(pointsLocal);
@@ -45,39 +43,38 @@ private:
   }
   __aicore__ inline void Compute(int32_t progress, int32_t b, int32_t c,
                                  int32_t n, int32_t m) {
-    AscendC::LocalTensor<half> pointsLocal = inQueuePoints.DeQue<half>();
-    AscendC::LocalTensor<half> idxLocal = inQueueIdx.DeQue<half>();
-    AscendC::LocalTensor<half> outLocal = outQueueOut.AllocTensor<half>();
+    AscendC::LocalTensor<int32_t> pointsLocal = inQueuePoints.DeQue<int32_t>();
+    AscendC::LocalTensor<int32_t> idxLocal = inQueueIdx.DeQue<int32_t>();
+    AscendC::LocalTensor<int32_t> outLocal = outQueueOut.AllocTensor<int32_t>();
     int32_t b_idx = progress / (m * c);
     int32_t c_idx = (progress / m) % c;
     int32_t m_idx = progress % m;
-    // outLocal[progress] = pointsLocal[b_idx * (c * n) + c_idx * n +
-    // idxLocal[b_idx * m + m_idx]];
-    outLocal.SetValue(pointsLocal.GetValue(
-        b_idx * (c * n) + c_idx * n + idxLocal.GetValue(b_idx * m + m_idx)));
-    outQueueZ.EnQue<half>(outLocal);
-    inQueueX.FreeTensor(pointsLocal);
-    inQueueY.FreeTensor(idxLocal);
+    int32_t index = idxLocal.GetValue(b_idx * m + m_idx);
+    int32_t value = pointsLocal.GetValue(b_idx * (c * n) + c_idx * n + index);
+    outLocal.SetValue(progress, value);
+    outQueueOut.EnQue<int32_t>(outLocal);
+    inQueuePoints.FreeTensor(pointsLocal);
+    inQueueIdx.FreeTensor(idxLocal);
   }
   __aicore__ inline void CopyOut(int32_t progress, int32_t b, int32_t c,
                                  int32_t n, int32_t m) {
-    AscendC::LocalTensor<half> outLocal = outQueueOut.DeQue<half>();
+    AscendC::LocalTensor<int32_t> outLocal = outQueueOut.DeQue<int32_t>();
     AscendC::DataCopy(outGm, outLocal, b * c * m);
-    outQueueZ.FreeTensor(outLocal);
+    outQueueOut.FreeTensor(outLocal);
   }
 
 private:
   AscendC::TPipe pipe;
   AscendC::TQue<AscendC::QuePosition::VECIN, 1> inQueuePoints, inQueueIdx;
   AscendC::TQue<AscendC::QuePosition::VECOUT, 1> outQueueOut;
-  AscendC::GlobalTensor<half> pointsGm;
-  AscendC::GlobalTensor<half> idxGm;
-  AscendC::GlobalTensor<half> outGm;
+  AscendC::GlobalTensor<int32_t> pointsGm;
+  AscendC::GlobalTensor<int32_t> idxGm;
+  AscendC::GlobalTensor<int32_t> outGm;
 };
 
 extern "C" __global__ __aicore__ void
-gather_points_kernel(int32_t b, int32_t c, int32_t n, int32_t m, GM_ADDR points,
-                     GM_ADDR idx, GM_ADDR out) {
+gather_points_kernel(int32_t b, int32_t c, int32_t n, int32_t m,
+                     uint32_t *points, uint32_t *idx, uint32_t *out) {
   KernelGatherPoints op;
   op.Init(b, c, n, m, points, idx, out);
   op.Process(b, c, n, m);
@@ -85,8 +82,9 @@ gather_points_kernel(int32_t b, int32_t c, int32_t n, int32_t m, GM_ADDR points,
 
 #ifndef ASCENDC_CPU_DEBUG
 void gather_points_do(uint32_t blockDim, void *stream, int32_t b, int32_t c,
-                      int32_t n, int32_t m, uint8_t *points, uint8_t *idx,
-                      uint8_t *out) {
-  gather_points_kernel<<<1, nullptr, stream>>>(b, c, n, m, points, idx, out);
+                      int32_t n, int32_t m, int32_t *points, int32_t *idx,
+                      int32_t *out) {
+  gather_points_kernel<<<blockDim, nullptr, stream>>>(b, c, n, m, points, idx,
+                                                      out);
 }
 #endif
