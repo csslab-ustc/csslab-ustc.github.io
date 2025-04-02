19bdaaa270e1b1b654232253e9944cb523b04b09
hbj <bjhua@ustc.edu.cn>
2025-04-02 20:21:22 +0800

    revised

---
+++ courses/theory/code/imo.py
@@ -0,0 +1,24 @@
+from z3 import *
+
+debug: bool = True
+
+########################################
+# Some IMO problems.
+
+# for any positive integer n
+def imo_1959_p1():
+    pass
+
+
+if __name__ == '__main__':
+    check(points[3], points[9])
+    check(points[0], points[9])
+
+
+
+
+
+
+
+
+
