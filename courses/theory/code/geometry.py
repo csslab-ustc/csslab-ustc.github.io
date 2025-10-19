f89c68a2ddd0f93c3d4766773de607e735ca1e12
hbj <bjhua@ustc.edu.cn>
2025-10-19 21:39:56 +0800

    revised

--- courses/theory/code/geometry.py
+++ courses/theory/code/geometry.py
@@ -1,102 +1,169 @@
-from typing import Callable, List
+from typing import Callable
 
-DEBUG = True
-
-'''
-我们研究线性及非线性约束的几何解释。
-'''
-
-import numpy as np
 import matplotlib.pyplot as plt
+from z3 import *
 
-'''
-绘制二维图形。
-我们采用暴力穷举法绘制图像，而不用解析形式，是因为一般情况下，（非线性）约束
-不满足函数形式。例如，椭圆的约束
-x^2    y^2
---- + ----- = 1
-a^2    b^2
-对同一个变量x的值，可能有两个（对称的）y值与之对应。
-'''
-def draw_pic_2d(from_: int,
-                to_: int,
-                stride: float,
-                relation: Callable[[float, float], bool],
-                name: str):
-    if DEBUG:
-        print(name)
+
+def draw(name: str,
+         x_min: int,
+         x_max: int,
+         y_min: int,
+         y_max: int,
+         stride: float,
+         predicate: Callable[[float, float], bool]):
     xs = []
-    i = from_
-    while i <= to_:
-        xs.append(i)
-        i += stride
-    print(xs)
-    ys = xs.copy()
-    if DEBUG:
-        print(xs)
-        print(ys)
-    result_xs: List[float] = []
-    result_ys: List[float] = []
-    x_axis = [0 for i in range(len(xs))]
-    plt.plot(xs, x_axis, linestyle='--', label="x_axis")
-    plt.plot(x_axis, xs, linestyle='--', label="y_axis")
+    r = x_min
+    while r <= x_max:
+        xs.append(r)
+        r += stride
+    ys = []
+    r = y_min
+    while r <= y_max:
+        ys.append(r)
+        r += stride
+    x_result = []
+    y_result = []
+    # x-axis and y-axis
+    plt.plot(xs, [0 for x in xs], label="x-axis", linestyle="--")
+    plt.plot([0 for x in ys], ys, label="y-axis", linestyle="--")
     for x in xs:
-        xq: float = (x / 4.0) ** 2
         for y in ys:
-            yq: float = (y / 3.0) ** 2
-            sumqq: float = xq + yq - 1.0
-            # print(xq, xqq, yq, yqq, delta, delta2)
-            # sys.exit(0)
-            if sumqq.__abs__() <= 1e-6:
-                result_xs.append(x)
-                result_ys.append(y)
-    if DEBUG:
-        print(result_xs)
-        print(result_ys)
-    plt.scatter(result_xs, result_ys, label=name)
+            if predicate(x, y):
+                x_result.append(x)
+                y_result.append(y)
+    plt.scatter(x_result, y_result, label=name, s=3)
     plt.xlabel("x")
     plt.ylabel("y")
     plt.legend()
-    plt.show()
-    # plt.savefig(name)
-    # plt.close()
+    plt.savefig(name)
+    plt.close()
 
-#
-# def float_eq(f1: Decimal, f2: Decimal, threshold: Decimal) -> bool:
-#     if Decimal.__abs__(f1-f2) <= threshold:
-#         return True
-#     return False
+
+def float_eq(f_1, f_2):
+    if abs(f_1 - f_2) < 1e-2:
+        return True
+    return False
 
 
 if __name__ == "__main__":
-    # draw_pic_2d(-10,
-    #             10,
-    #             1e-2,
-    #             lambda x, y: float_eq(Decimal..(x),
-    #                                   y,
-    #                                   1e-3),
-    #             "sin")
-    # draw_pic_2d(-10,
-    #             10,
-    #             1e-2,
-    #             lambda x, y: float_eq(math.cos(x),
-    #                                   y,
-    #                                   1e-3),
-    #             "cos")
-    draw_pic_2d(-10,
-                10,
-                10*0.001,
-                lambda x, y: True,
-                "ellipse")
+    draw("line",
+         -10,
+         10,
+         -10,
+         10,
+         1e-2,
+         lambda x, y: float_eq(2 * x + 3 - y, 0))
+    draw("sin",
+         -10,
+         10,
+         -2,
+         2,
+         1e-2,
+         lambda x, y: float_eq(math.sin(x) - y, 0))
+    draw("cos",
+         -10,
+         10,
+         -2,
+         2,
+         1e-2,
+         lambda x, y: float_eq(math.cos(x) - y, 0))
+    draw("exp",
+         -10,
+         10,
+         -10,
+         10,
+         1e-2,
+         lambda x, y: float_eq(math.exp(x) - y, 0))
+    draw("sigmoid",
+         -10,
+         10,
+         -2,
+         2,
+         1e-2,
+         lambda x, y: float_eq(1 / (1 + math.exp(-x)) - y, 0))
+    draw("circle",
+         -10,
+         10,
+         -10,
+         10,
+         1e-2,
+         lambda x, y: float_eq(math.pow(x, 2) + math.pow(y, 2) - 5, 0))
+    draw("ellipse",
+         -10,
+         10,
+         -10,
+         10,
+         1e-2,
+         lambda x, y: float_eq(math.pow(x, 2) / 4 + math.pow(y, 2) / 2 - 5, 0))
+    '''
+    Elliptic curves (general form):
+    y^2 = x^3 + ax + b
+    here we draw some elliptic curves for different combination of the
+    coefficients a and b:
+    '''
+    for a in range(-2, 3, 1):
+      for b in range(-2, 3, 1):
+        draw(f"elliptic_curve_a={a}_b={b}",
+          -10,
+          10,
+          -10,
+          10,
+          1e-2,
+          lambda x, y: float_eq(math.pow(x, 3) + a*x + b -math.pow(y, 2), 0))
+    draw(f"format",
+          -10,
+          10,
+          -10,
+          10,
+          1e-3,
+          lambda x, y: float_eq(math.pow(x, 3) + math.pow(y, 3) - 1, 0))
+
+    #     # try constraint solving with Z3
+    #     solver = Solver()
+    #     x, y = Reals('x y')
+    #     solver.add(x**3+a*x +b -y**2==0)
+    #     res = solver.check()
+    #     if res == sat:
+    #       print(f"y^2=x^3+{a}x+{b}", solver.model())
+    #     else:
+    #       print("unsat")
 
 
-
-
-
-
-
-
-
+    # we can try another way to draw figures
+    # by leveraging a solver:
+    # solver = Solver()
+    # x, y = Reals('x y')
+    # solver.add(x ** 3 + y ** 3 - 1 == 0)
+    # num_of_solutions = 0
+    # xs = []
+    # ys = []
+    # while num_of_solutions < 1000:
+    #     res = solver.check()
+    #     if res == sat:
+    #         num_of_solutions += 1
+    #         # print(f"=x^3+y^3==1", solver.model())
+    #         x_value = solver.model()[x]
+    #         y_value = solver.model()[y]
+    #         x_str: str = x_value.as_decimal(10)
+    #         x_f = float(x_str.strip('?'))
+    #         xs.append(x_f)
+    #         y_str: str = y_value.as_decimal(10)
+    #         y_f = float(y_str.strip('?'))
+    #         ys.append(y_f)
+    #         solver.add(Not((x == x_value)))
+    #     else:
+    #         print("unsat")
+    #         break
+    # # print(xs)
+    # # print(ys)
+    # plt.scatter(xs,
+    #             ys,
+    #             label="sat", s=3)
+    # plt.xlabel("x")
+    # plt.ylabel("y")
+    # plt.legend()
+    # plt.savefig("sat")
+    # plt.close()
 
 
 
